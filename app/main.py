3ab8ec75968cf96a6ce6777b559530a79a791888
Bohdan <bogdanbelyj@gmail.com>
2025-09-25 11:10:56 -0600

    exercise fixed third time

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int,
+    def __init__(self, distance_from_city_center: float,
                  average_rating: float, clean_power: float,
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
@@ -15,14 +15,14 @@ class CarWashStation:
         self.clean_power = clean_power
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
 
-        return income
+        return round(income, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
@@ -37,18 +37,6 @@ class CarWashStation:
     def rate_service(self, rate: float) -> None:
         new_count = self.count_of_ratings + 1
         total = self.count_of_ratings * self.average_rating + rate
-        self.average_rating = round((total / new_count), 1)
+        self.average_rating = total / new_count
+        self.average_rating = round(self.average_rating, 1)
         self.count_of_ratings = new_count
-
-
-bmw = Car(comfort_class=3, clean_mark=3, brand="BMW")
-audi = Car(comfort_class=4, clean_mark=2, brand="Audi")
-ws = CarWashStation(5, 3.5, 6, 6)
-print(ws.calculate_washing_price(bmw))
-print(ws.calculate_washing_price(audi))
-print(ws.serve_cars([bmw, audi]))
-print(audi.clean_mark)
-print(bmw.clean_mark)
-ws.rate_service(5)
-print(ws.count_of_ratings)
-print(ws.average_rating)
